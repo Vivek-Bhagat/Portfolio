78c388e995745574224ae387898ea8591f139e89
Vivek Bhagat <vivekbhagat1890@hotmail.com>
2023-02-09 23:40:28 +0530

    changes not made

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// var load = document.getElementById("loading");
+// function loadfun() {
+//     load.style.display = 'none';
+// }
+
+
+
+
+// let loader = document.getElementById("pre-loader")
+// window.addEventListener("load", function () {
+//     loader.style.display = "none";
+// });
